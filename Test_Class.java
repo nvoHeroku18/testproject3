3cd8c0b004b47121cc6ea42c6e8bc990f817f78c
nvoHeroku18 <37953966+nvoHeroku18@users.noreply.github.com>
2018-03-31 15:50:15 +0200

    Add files via upload

---
+++ Test_Class.java
@@ -0,0 +1,10 @@
+package Hello_World;
+
+public class Test_Class {
+	
+	public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+        System.exit( 0 ); //success
+    }
+
+}
